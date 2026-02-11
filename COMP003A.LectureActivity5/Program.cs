6e3e612600708087f38d8a8dba5d8890297e3915
samian <sam.iannazzo@yahoo.com>
2026-02-10 23:01:07 -0800

    created method DisplayUserInfo

--- COMP003A.LectureActivity5/Program.cs
+++ COMP003A.LectureActivity5/Program.cs
@@ -5,10 +5,20 @@
         static void Main(string[] args)
         {
             DisplayWelcome();
+
+            string name = "Sam";
+
+            int age = 29;
+
+            DisplayUserInfo($"Name: {name} \nAge: {age}");
         }
         static void DisplayWelcome()
         {
             Console.WriteLine("Welcome to Module 5!");
+        }
+        static void DisplayUserInfo(string message)
+        {
+            Console.WriteLine(message);
         }
     } 
 }

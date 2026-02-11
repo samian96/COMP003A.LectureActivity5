ad3410234777e5e204943b95176be34d2a7bdcd4
samian <sam.iannazzo@yahoo.com>
2026-02-10 23:17:33 -0800

    static int CalculateSum added, had trouble with declaring

--- COMP003A.LectureActivity5/Program.cs
+++ COMP003A.LectureActivity5/Program.cs
@@ -10,7 +10,15 @@
 
             int age = 29;
 
+            int num1 = 5;
+
+            int num2 = 7;
+
+            int sum = CalculateSum(num1, num2);
+
             DisplayUserInfo($"Name: {name}, Age: {age}");
+
+            Console.WriteLine("The sum is:" + "" + sum);
         }
         static void DisplayWelcome()
         {
@@ -19,6 +27,10 @@
         static void DisplayUserInfo(string message)
         {
             Console.WriteLine(message);
+        }
+        static int CalculateSum(int num1, int num2)
+        {
+            return num1 + num2;
         }
     } 
 }
